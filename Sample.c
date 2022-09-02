97d215e2686fae9626a6a5cc64f64ce5c0d09140
MEENUGULA UMA <111738093+204g1a05b5@users.noreply.github.com>
2022-09-02 16:07:47 +0530

    my first C file is created

---
+++ Sample.c
@@ -0,0 +1,10 @@
+/* Name Of the Candidate:M.Uma
+   Roll No: 204G1A05b5
+   Name of the experiment:exp1
+*/
+  // Source Code
+    #include <stdio.h>
+    int main(){
+	    printf("Hello World\n");
+      return 0;
+    }
