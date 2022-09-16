d89be33ced2b46d3657b1772b6361402450b9ba7
MEENUGULA UMA <111738093+204g1a05b5@users.noreply.github.com>
2022-09-16 14:48:57 +0530

    create a EXP1 successfully

--- OSLAB/EXP1/FCFS/FCFS.c
+++ OSLAB/EXP1/FCFS/FCFS.c
@@ -1,4 +1,10 @@
-//Name of the student:M UMA
+/*Name of the student:M UMA
 Roll no: 204G1A05B5
 Experiment Name: Implementing of ip address
-//
+Date of experiment: 16-09-2022
+*/
+// Source Code
+    #include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
