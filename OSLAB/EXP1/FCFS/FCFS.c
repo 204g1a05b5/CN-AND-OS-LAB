83df6200c086a643cd331a1c367898e7ed38deaa
MEENUGULA UMA <111738093+204g1a05b5@users.noreply.github.com>
2022-09-16 15:15:14 +0530

    FCFS.c file created

--- OSLAB/EXP1/FCFS/FCFS.c
+++ OSLAB/EXP1/FCFS/FCFS.c
@@ -1,9 +1,3 @@
-/*Name of the student:M UMA
-Roll no: 204G1A05B5
-Experiment Name: Implementing of ip address
-Date of experiment: 16-09-2022
-*/
-// Source Code
     #include <stdio.h>
     int main(){
 	printf("Hello World\n");
